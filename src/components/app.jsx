d0219a8bbe7b79b34b17573c03dcd88174a101da
ktanaha <moutoku1984@gmail.com>
2018-08-29 07:27:02 +0900

    section3 recture23

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,34 +1,23 @@
-import React, { Component } from 'react';
-
-import Greeting from './greeting';
+import React, { Component } from "react";
+import SearchForm from "./SearchForm";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: 'Bob',
-    };
+    this.state = {};
   }
 
-  handleNameChange(name) {
-    this.setState({ name });
-  }
-
-  handleBobClicked() {
-    this.setState({ name: 'Bob' });
+  handlePlaceSubmit(place) {
+    console.log(place);
   }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={e => this.handleNameChange(e.target.value)}
-        />
-        <button onClick={() => this.handleBobClicked()}> I am Bob</button>
-        <Greeting name={this.state.name} />
-      </div>);
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+      </div>
+    );
   }
 }
 
