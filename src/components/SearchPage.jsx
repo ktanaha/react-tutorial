d4310963857fd9eb1070970d54ec6c06a72be164
ktanaha <moutoku1984@gmail.com>
2018-09-08 11:21:43 +0900

    section3 recture52

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
-import queryString from 'query-string';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
+import queryString from "query-string";
 
-import SearchForm from '../components/SearchForm';
+import SearchForm from "../containers/SearchForm";
 
-import { geocode } from '../domain/Geocoder';
-import { searchHotelByLocation } from '../domain/HotelRepository';
+import { geocode } from "../domain/Geocoder";
+import { searchHotelByLocation } from "../domain/HotelRepository";
 
 const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey]);
 
@@ -14,27 +14,20 @@ class SearchPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      place: this.getPlaceParam() || '東京タワー',
+      place: this.getPlaceParam() || "東京タワー",
       location: {
         lat: 35.6585805,
-        lng: 139.7454329,
+        lng: 139.7454329
       },
-      sortKey: 'price',
+      sortKey: "price"
     };
   }
 
   componentDidMount() {
-    this.unsubscrinbe = this.props.store.subscribe(() => {
-      this.forceUpdate();
-    });
     //   const place = this.getPlaceParam();
     //   if (place) {
     //     this.startSearch(place);
     //   }
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
   }
 
   getPlaceParam() {
@@ -51,14 +44,9 @@ class SearchPage extends Component {
       address: message,
       location: {
         lat: 0,
-        lng: 0,
-      },
+        lng: 0
+      }
     });
-  }
-
-  handlePlaceChange(e) {
-    e.preventDefault();
-    this.props.store.dispatch({ type: 'CHANGE_PLACE', place: e.target.value });
   }
 
   handlePlaceSubmit(e) {
@@ -71,45 +59,41 @@ class SearchPage extends Component {
     geocode(this.state.place)
       .then(({ status, address, location }) => {
         switch (status) {
-          case 'OK': {
+          case "OK": {
             this.setState({ address, location });
             return searchHotelByLocation(location);
           }
-          case 'ZERO_RESULTS': {
-            this.setErrorMessage('見つかりませんでした');
+          case "ZERO_RESULTS": {
+            this.setErrorMessage("見つかりませんでした");
             break;
           }
           default: {
-            this.setErrorMessage('エラーが発生しました');
+            this.setErrorMessage("エラーが発生しました");
           }
         }
         return [];
       })
-      .then((hotels) => {
+      .then(hotels => {
         this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) });
       })
       .catch(() => {
-        this.setErrorMessage('通信に失敗しました');
+        this.setErrorMessage("通信に失敗しました");
       });
   }
 
   handleSortKeyChange(sortKey) {
     this.setState({
       sortKey,
-      hotels: sortedHotels(this.state.hotels, sortKey),
+      hotels: sortedHotels(this.state.hotels, sortKey)
     });
   }
 
   render() {
-    const state = this.props.store.getState();
+    console.log(this.props);
     return (
       <div className="search-page">
         <h1 className="app-title">ホテル検索</h1>
-        <SearchForm
-          place={state.place}
-          onPlaceChange={e => this.handlePlaceChange(e)}
-          onSubmit={e => this.handlePlaceSubmit(e)}
-        />
+        <SearchForm onSubmit={e => this.handlePlaceSubmit(e)} />
         {/*
         <div className="result-area">
           <Map location={this.state.location} />
@@ -134,12 +118,7 @@ class SearchPage extends Component {
 
 SearchPage.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func }).isRequired,
-  location: PropTypes.shape({ search: PropTypes.string }).isRequired,
-  store: PropTypes.shape({
-    subscribe: PropTypes.func,
-    getState: PropTypes.func,
-    dispatch: PropTypes.func,
-  }).isRequired,
+  location: PropTypes.shape({ search: PropTypes.string }).isRequired
 };
 
 export default SearchPage;
--- src/containers/SearchForm.jsx
+++ src/containers/SearchForm.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+  place: state.place,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onPlaceChange: place => dispatch({ type: 'CHANGE_PLACE', place }),
+});
+
 const SearchForm = props => (
   <form className="search-form" onSubmit={e => props.onSubmit(e)}>
     <input
@@ -8,7 +18,10 @@ const SearchForm = props => (
       type="text"
       size="30"
       value={props.place}
-      onChange={e => props.onPlaceChange(e)}
+      onChange={(e) => {
+        e.preventDefault();
+        props.onPlaceChange(e.target.value);
+      }}
     />
     <input className="submit-button" type="submit" value="検索" />
   </form>
@@ -20,4 +33,9 @@ SearchForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
 
-export default SearchForm;
+const ConnectedSearchForm = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SearchForm);
+
+export default ConnectedSearchForm;
